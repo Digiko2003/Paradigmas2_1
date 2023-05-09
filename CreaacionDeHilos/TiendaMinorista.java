919fdc604d2133e45ebcb399ab56bda97a4689b1
Diego Arturo Enriquez Mercado <93006286+Digiko2003@users.noreply.github.com>
2023-05-09 15:50:55 -0600

    09-05

--- CreaacionDeHilos/TiendaMinorista.java
+++ CreaacionDeHilos/TiendaMinorista.java
@@ -6,8 +6,6 @@ package CreaacionDeHilos;
             ArticuloMinorista articulo2 = new ArticuloMinorista("Jeans de diseñador ", 40, 34.95);
             ArticuloMinorista articulo3 = new ArticuloMinorista("Camisa", 20, 24.95);
 
-
-
             System.out.println("Descripcion: "+ articulo1.getDescripcion()+ " - Unidades disponibles: " + articulo1.getUnidadesDisponibles() + " - Precio: " + articulo1.getPrecio());
             System.out.println("Descripcion: "+ articulo2.getDescripcion()+ " - Unidades disponibles: " + articulo2.getUnidadesDisponibles() + " - Precio: " + articulo2.getPrecio());
             System.out.println("Descripcion: "+ articulo3.getDescripcion()+ " - Unidades disponibles: " + articulo3.getUnidadesDisponibles() + " - Precio: " + articulo3.getPrecio());
